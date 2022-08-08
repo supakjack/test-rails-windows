f772f5b3730787c2c3fa3eed8b4513e5d140e064
supakjack <supakjack@gmail.com>
2022-08-08 23:32:37 +0700

    test

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, :omniauth_providers => [:google_oauth2]
+
+    def self.from_omniauth(access_token)
+      data = access_token.info
+      user = User.where(:email => data["email"]).first
+    
+      unless user
+        password = Devise.friendly_token[0,20]
+        user = User.create(name: data["name"], email: data["email"],
+          password: password, password_confirmation: password
+        )
+      end
+      user
+    end
+        
+end
