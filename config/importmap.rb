cc37d9025ca992b2d096c5561f920e30c6363c4d
Theeraphat Jantakat <pppangpondpp7@gmail.com>
2022-08-08 23:57:34 +0700

    added turbo-rails

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,4 +2,5 @@
 
 pin "application", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "@hotwired/turbo-rails"
 pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.0.3-1/lib/assets/compiled/rails-ujs.js"
